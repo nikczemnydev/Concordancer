09ae28737f4b53b1d759e985cf977e5dadda696f
Rafal <136376818+nikczemnydev@users.noreply.github.com>
2023-12-13 18:49:59 +0100

    Fixed some brackets not enclosing what they should.

--- src/Concordancer.java
+++ src/Concordancer.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Concordancer {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         String fileName = "c:\\mobydick.txt";
         List<String> tokens = new ArrayList<>();
@@ -15,8 +15,7 @@ public class Concordancer {
                 tokens.add(scanner.next());
             }
             scanner.close();
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found: " + fileName);
             return;
         }
@@ -25,45 +24,45 @@ public class Concordancer {
         System.out.println("Enter a search phrase please (type .stop to quit)");
         String phrase = input.nextLine();
 
-         while (!phrase.equals(".stop")) {
-             List<Context> contexts = findContexts(tokens, phrase);
+        while (!phrase.equals(".stop")) {
+            List<Context> contexts = findContexts(tokens, phrase);
 
-             System.out.println(contexts.size() + "individual contexts found in total");
+            System.out.println(contexts.size() + "individual contexts found in total");
 
-             System.out.println("<<<<<<<<<< Left contexts <<<<<");
-             for (Context context : contexts) {
-                 System.out.println(context.left);
-             }
+            System.out.println("<<<<<<<<<< Left contexts <<<<<");
+            for (Context context : contexts) {
+                System.out.println(context.left);
+            }
 
-             System.out.println(">>>>> Right contexts >>>>>>>>>>");
-             for (Context context : contexts) {
-                 System.out.println(context.right);
-             }
+            System.out.println(">>>>> Right contexts >>>>>>>>>>");
+            for (Context context : contexts) {
+                System.out.println(context.right);
+            }
 
-             System.out.println("<<<<<<< Bidirectional contexts >>>>>>>");
-             for (Context context : contexts) {
-                 System.out.println(context.getBidirectional());
-             }
-
-         }
-
-        System.out.println("Please enter a search phrase (.stop to end the program):");
-        phrase = input.nextLine();
-    }
-}
-
-static class Context {
-    String left;
-    String right;
-    String phrase;
-
-    public Context(String left, String right, String phrase) {
-        this.left = left;
-        this.right = right;
-        this.phrase = phrase;
+            System.out.println("<<<<<<< Bidirectional contexts >>>>>>>");
+            for (Context context : contexts) {
+                System.out.println(context.getBidirectional());
+            }
+            System.out.println("Please enter a search phrase (.stop to end the program):");
+            phrase = input.nextLine();
+        }
+        input.close();
     }
 
-    public String getBidirectional() {
-        return left + phrase + right;
+
+    static class Context {
+        String left;
+        String right;
+        String phrase;
+
+        public Context(String left, String right, String phrase) {
+            this.left = left;
+            this.right = right;
+            this.phrase = phrase;
+        }
+
+        public String getBidirectional() {
+            return left + phrase + right;
+        }
     }
 }

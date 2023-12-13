1652ae40ef912c060c87d28c6060c0e23358927a
Rafal <136376818+nikczemnydev@users.noreply.github.com>
2023-12-13 19:04:11 +0100

    Fixed some more bracketing, added helper method that does all the heavy lifting. The code now compiles.

--- src/Concordancer.java
+++ src/Concordancer.java
@@ -2,9 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Concordancer {
-
     public static void main(String[] args) {
-
         String fileName = "c:\\mobydick.txt";
         List<String> tokens = new ArrayList<>();
 
@@ -26,19 +24,15 @@ public class Concordancer {
 
         while (!phrase.equals(".stop")) {
             List<Context> contexts = findContexts(tokens, phrase);
-
             System.out.println(contexts.size() + "individual contexts found in total");
-
             System.out.println("<<<<<<<<<< Left contexts <<<<<");
             for (Context context : contexts) {
                 System.out.println(context.left);
             }
-
             System.out.println(">>>>> Right contexts >>>>>>>>>>");
             for (Context context : contexts) {
                 System.out.println(context.right);
             }
-
             System.out.println("<<<<<<< Bidirectional contexts >>>>>>>");
             for (Context context : contexts) {
                 System.out.println(context.getBidirectional());
@@ -48,8 +42,6 @@ public class Concordancer {
         }
         input.close();
     }
-
-
     static class Context {
         String left;
         String right;
@@ -60,9 +52,33 @@ public class Concordancer {
             this.right = right;
             this.phrase = phrase;
         }
-
         public String getBidirectional() {
             return left + phrase + right;
         }
+    }
+    public static List<Context> findContexts(List<String> tokens, String phrase) {
+        List<Context> contexts = new ArrayList<>();
+        String[] words = phrase.split("\\s+");
+        for (int i = 0; i < tokens.size() - words.length + 1; i++) {
+            boolean match = true;
+            for (int j = 0; j < words.length; j++) {
+                if (!tokens.get(i + j).equals(words[j])) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                String left = "";
+                for (int k = Math.max(0, i - 5); k < i; k++) {
+                    left += tokens.get(k) + " ";
+                }
+                String right = "";
+                for (int k = i + words.length; k < Math.min(tokens.size(), i + words.length + 5); k++) {
+                    right += tokens.get(k) + " ";
+                }
+                contexts.add(new Context(left, right, phrase));
+            }
+        }
+        return contexts;
     }
 }
